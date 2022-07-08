f5454a8f10ef380b4aed885c75cef7040e9ae080
Ovri <hello@ovri.com>
2022-07-08 12:42:42 +0200

    Release 1.5.2

---
+++ class-wc-ovri.php
@@ -0,0 +1,374 @@
+<?php
+class WC_Ovri extends WC_Payment_Gateway
+{
+  /**
+   * Construction of the classical method
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    global $woocommerce;
+    $this->version = ovri_universale_params()['Version'];
+    $this->id = 'ovri';
+
+    $this->icon = ovri_get_file("assets/img/carte.png");
+
+    $this->init_form_fields();
+
+
+    // Load the settings.
+    $this->init_settings();
+    $this->has_fields = false;
+
+    $this->method_title = __('Ovri', 'ovri');
+
+
+    // Define user set variables.
+    $this->title = $this->settings['title'];
+    $this->instructions = $this->get_option('instructions');
+    $this->method_description = __('Accept credit cards in less than 5 minutes. <a href="https://my.ovri.app">Open an account now !</a>', 'ovri');
+    $this->ovri_gateway_api_key = $this->settings['ovri_gateway_api_key'];
+    $this->ovri_gateway_secret_key = $this->settings['ovri_gateway_secret_key'];
+
+    // Actions.
+    if (version_compare(WOOCOMMERCE_VERSION, '2.0.0', '>=')) {
+      add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(&$this, 'process_admin_options'));
+    } else {
+      add_action('woocommerce_update_options_payment_gateways', array(&$this, 'process_admin_options'));
+    }
+    // Add listener IPN Function
+    add_action('woocommerce_api_wc_ovri', array($this, 'ovri_notification'));
+    // Add listener Customer Return after payment and IPN result !
+    add_action('woocommerce_api_wc_ovri_return', array($this, 'ovri_return'));
+  }
+  /* Admin Panel Options.*/
+  public function admin_options()
+  {
+?>
+    <h3>
+      <?php _e('Ovri configuration', 'ovri'); ?>
+    </h3>
+    <div class="simplify-commerce-banner updated"> <img src="<?php echo ovri_get_file("assets/img/ovri.png"); ?>" />
+      <p class="main"><strong><?php echo __('Accepts payments by credit card with Ovri', 'ovri'); ?></strong></p>
+      <p><?php echo __('Ovri is a secure payment solution on the Internet. As a virtual POS (Electronic Payment Terminal), Ovri makes it possible to cash payments made on the Internet 24 hours a day, 7 days a week. This service relieves your site of the entire payment phase; the latter takes place directly on our secure payment platform.', 'ovri'); ?></p>
+      <p><?php echo __('For any problem or information contact: hello@ovri.app', 'ovri'); ?></p>
+      <p><a href="https://my.ovri.app" target="_blank" class="button button-primary"><?php echo __('Get a Ovri account', 'ovri'); ?></a> <a href="https://my.ovri.app" target="_blank" class="button"><?php echo __('Test free', 'ovri'); ?></a> <a href="https://www.ovri.com" target="_blank" class="button"><?php echo __('Official site', 'ovri'); ?></a> <a href="https://docs.ovri.app" target="_blank" class="button"><?php echo __('Documentation', 'ovri'); ?></a></p>
+    </div>
+    <div class="simplify-commerce-banner error">
+      <p class="main" style="color:red;"><strong> <?php echo __('If you want your customer to be automatically redirected to your site once the payment is accepted or failed, consider activating this option directly in the configuration of your website in your Ovri DashBoard', 'ovri'); ?> </strong></p>
+    </div>
+    <table class="form-table">
+      <?php $this->generate_settings_html(); ?>
+    </table>
+<?php
+  }
+  public function ovri_notification()
+  {
+    global $woocommerce;
+    if (isset($_POST['TransId'])) {
+      $TransactionId = sanitize_text_field($_POST['TransId']);
+    } else {
+      /* Display for DEBUG Mod */
+      echo "The transaction id is not transmitted";
+      error_log('Ovri IPN Error : The transaction id is not transmitted');
+      exit();
+    }
+
+    $Request = $this->signRequest(array(
+      'ApiKey' => $this->ovri_gateway_api_key,
+      'TransID' => $TransactionId
+    ));
+
+    $result = json_decode($this->getTransactions($Request)['body'], true);
+    if (isset($result['ErrorCode'])) {
+      /* If error return error log for mode debug and stop process */
+      echo 'Ovri IPN Error : ' . esc_attr($result['ErrorCode']) . '-' . esc_attr($result['ErrorDescription']) . '';
+      error_log('Ovri IPN Error : ' . $result['ErrorCode'] . '-' . $result['ErrorDescription'] . '');
+      exit();
+    }
+    $order = new WC_Order($result['Merchant_Order_Id']);
+    //check if order exist
+    if (!$order) {
+      echo 'Ovri IPN Error : Order ' . esc_attr($result['Merchant_Order_Id']) . ' not found';
+      error_log('Ovri IPN Error : Order ' . $result['Merchant_Order_Id'] . ' not found');
+      exit();
+    }
+    //check if already paid
+    if ($order->is_paid()) {
+      echo 'Ovri IPN Error : Order ' . esc_attr($result['Merchant_Order_Id']) . ' already paid';
+      error_log('Ovri IPN Error : Order ' . $result['Merchant_Order_Id'] . ' already paid');
+      exit();
+    }
+    //check if already completed
+    if ($order->has_status('completed')) {
+      echo 'Ovri IPN Error : Order ' . esc_attr($result['Merchant_Order_Id']) . ' is completed';
+      error_log('Ovri IPN Error : Order ' . $result['Merchant_Order_Id'] . ' is completed');
+      exit();
+    }
+    //check if already processing
+    if ($order->has_status('processing')) {
+      echo 'Ovri IPN Error : Order ' . esc_attr($result['Merchant_Order_Id']) . ' is in processig';
+      error_log('Ovri IPN Error : Order ' . $result['Merchant_Order_Id'] . ' is in processing');
+      exit();
+    }
+    //check if refunded
+    if ($order->has_status('refunded')) {
+      echo 'Ovri IPN Error : Order ' . esc_attr($result['Merchant_Order_Id']) . ' is refunded';
+      error_log('Ovri IPN Error : Order ' . $result['Merchant_Order_Id'] . ' is refunded');
+      exit();
+    }
+    if ($result['Transaction_Status']['State'] == 2) {
+      $order->payment_complete($result['Bank']['Internal_IPS_Id']);
+      //reduce stock
+      wc_maybe_reduce_stock_levels($result['Merchant_Order_Id']);
+      //add note to order
+      $order->add_order_note('Payment by Ovri credit card accepted (IPN)', true);
+      //empty basket
+      $woocommerce->cart->empty_cart();
+      echo 'Order ' . esc_attr($result['Merchant_Order_Id']) . ' was successfully completed !';
+      exit();
+    } else {
+      //declined or cancelled
+      $order->update_status('failed', __('Ovri - Transaction ' . $TransactionId . ' - FAILED (' . $result['Transaction_Status']['Bank_Code_Description'] . ')', 'ovri'));
+      echo 'Order ' . esc_attr($result['Merchant_Order_Id']) . ' was successfully cancelled !';
+      exit();
+    }
+
+    echo "Unknown error";
+    exit();
+  }
+
+  public function ovri_return()
+  {
+    global $woocommerce;
+
+    /*Default Url*/
+    $returnUri = wc_get_checkout_url();
+    /*Securing*/
+    $order_id = sanitize_text_field($_GET['mtg_ord']);
+    /*Prevalidate*/
+    if ($order_id < 1) {
+      return;
+    }
+    /*Validation*/
+    $WcOrder = new WC_Order($order_id);
+    if (!$WcOrder) {
+      return;
+    };
+
+    /*Check if the payment method is Ovri for this order */
+    if ($WcOrder->get_payment_method() !== "ovri" && $WcOrder->get_payment_method() !== "ovripnftwo" & $WcOrder->get_payment_method() !== "ovripnfthree" && $WcOrder->get_payment_method() !== "ovripnffour") {
+      return;
+    }
+
+
+    /*Checking Order Status*/
+    if ($WcOrder->get_status() === 'pending') {
+
+      /* If the order is still pending, then we call the Ovri webservice to check the payment again and update the order status */
+      $Request = $this->signRequest(array(
+        'ApiKey' => $this->ovri_gateway_api_key,
+        'MerchantOrderId' => $order_id
+      ));
+      $checkTransaction = json_decode($this->getPaymentByOrderID($Request)['body'], true);
+
+      /* If an error code is returned then we redirect the client indicating the problem */
+      if (isset($checkTransaction['ErrorCode'])) {
+        error_log('Ovri API Error @ovri_return : ' . $checkTransaction['ErrorCode'] . ' : ' . $checkTransaction['ErrorDescription'] . '');
+        wc_add_notice(__('An internal error occurred', 'ovri'), 'error');
+      }
+
+      /* All is ok so we finish the final process */
+      $transactionStatuts = $checkTransaction['Transaction_Status']['State'];
+      if ($transactionStatuts == "2") {
+        /* transaction approved */
+        /* Record the payment with the transaction number */
+        $WcOrder->payment_complete($checkTransaction['Bank']['Internal_IPS_Id']);
+        /* Reduction of the stock */
+        wc_reduce_stock_levels($order_id);
+        /* Add a note on the order to say that the order is confirmed */
+        $WcOrder->add_order_note('Payment by Ovri credit card accepted', true);
+
+        /* We empty the basket */
+        $woocommerce->cart->empty_cart();
+        $returnUri = $this->get_return_url($WcOrder);
+      } else {
+        if ($transactionStatuts == "6") {
+          /* The transaction is still pending */
+          /* A message is displayed to the customer asking him to be patient */
+          /* We make it wait 10 seconds then we refresh the page */
+          echo __('Please wait a few moments ...', 'ovri');
+          header("Refresh:10");
+          exit();
+        } else {
+          /* La transaction est annulé ou refusé */
+          /* The customer is redirected to the shopping cart page with the rejection message */
+          /* Redirect the customer to the shopping cart and indicate that the payment is declined */
+          wc_add_notice(__('Sorry, your payment was declined !', 'ovri'), 'error');
+        }
+      }
+    } else {
+
+      /* The answer from ovri has already arrived (IPN) */
+      /* Redirect the customer to the thank you page if the order is well paid */
+      /* Fixed also redirects the customer to the acceptance page if the order has a completed status, useful for self-delivered products */
+      if ($WcOrder->get_status() === 'processing' || $WcOrder->get_status() === 'completed') {
+        $returnUri = $this->get_return_url($WcOrder);
+      } else {
+        /* Redirect the customer to the shopping cart and indicate that the payment is declined */
+        wc_add_notice(__('Sorry, your payment was declined !', 'ovri'), 'error');
+        /* force create new order for new attempts */
+        WC()->session->set('order_awaiting_payment', false);
+      }
+    }
+
+    /* Redirect to thank you or decline page */
+    wp_redirect($returnUri);
+    exit();
+  }
+
+
+  /**  
+   * Initialise Gateway Settings Form Fields for ADMIN. 
+   */
+  public function init_form_fields()
+  {
+    global $woocommerce;
+
+
+    $this->form_fields = array(
+      'enabled' => array(
+        'title' => __('Enable / Disable', 'ovri'),
+        'type' => 'checkbox',
+        'label' => __('Activate card payment with Ovri', 'ovri'),
+        'default' => 'no'
+      ),
+      'title' => array(
+        'title' => __('Method title', 'ovri'),
+        'type' => 'text',
+        'description' => __('This is the name displayed on your checkout', 'ovri'),
+        'desc_tip' => true,
+        'default' => __('Credit card payment', 'ovri')
+      ),
+      'description' => array(
+        'title' => __('Message before payment', 'ovri'),
+        'type' => 'textarea',
+        'description' => __('Message that the customer sees when he chooses this payment method', 'ovri'),
+        'desc_tip' => true,
+        'default' => __('You will be redirected to our secure server to make your payment', 'ovri')
+      ),
+      'ovri_gateway_api_key' => array(
+        'title' => __('Your API Key', 'ovri'),
+        'description' => __('To obtain it go to the configuration of your merchant contract (section "Merchant account").', 'ovri'),
+        'type' => 'text'
+      ),
+      'ovri_gateway_secret_key' => array(
+        'title' => __('Your Secret Key', 'ovri'),
+        'description' => __('To obtain it go to the configuration of your merchant contract (section "Merchant account").', 'ovri'),
+        'type' => 'text'
+      )
+    );
+  }
+
+  /**
+   * Retrieve transaction details from the transaction PSP id
+   */
+  private function getTransactions($arg)
+  {
+    $response = wp_remote_get(ovri_universale_params()['ApiGetTransaction'] . '?TransID=' . $arg["TransID"] . '&SHA=' . $arg["SHA"] . '&ApiKey=' . $arg["ApiKey"] . '');
+    return $response;
+  }
+
+  /** 
+   * Retrieve transaction details from the order ID
+   */
+  private function getPaymentByOrderID($arg)
+  {
+
+
+    $response = wp_remote_get(ovri_universale_params()['ApiGetTransactionByOrderId'] . '?MerchantOrderId=' . $arg["MerchantOrderId"] . '&SHA=' . $arg["SHA"] . '&ApiKey=' . $arg["ApiKey"] . '');
+    return $response;
+  }
+  /** 
+   * Request authorization token from Ovri 
+   * Private function only accessible to internal execution 
+   */
+  private function getToken($args)
+  {
+    $ConstructArgs = array(
+      'headers' => array(
+        'Content-type: application/x-www-form-urlencoded'
+      ),
+      'sslverify' => false,
+      'body' => $this->signRequest($args)
+    );
+    $response = wp_remote_post(ovri_universale_params()['ApiInitPayment'], $ConstructArgs);
+    return $response;
+  }
+  /* Signature of parameters with your secret key before sending to Ovri */
+  /* Private function only accessible to internal execution */
+  private function signRequest($params, $beforesign = "")
+  {
+    $ShaKey = $this->ovri_gateway_secret_key;
+    foreach ($params as $key => $value) {
+      $beforesign .= $value . "!";
+    }
+    $beforesign .= $ShaKey;
+    $sign = hash("sha512", base64_encode($beforesign . "|" . $ShaKey));
+    $params['SHA'] = $sign;
+    return $params;
+  }
+
+
+  /**
+   * Payment processing and initiation
+   * Redirection of the client if the initiation is successful
+   * Display of failures on the checkout page in case of error
+   */
+  public function process_payment($order_id)
+  {
+    //obtain token for payment processing
+    global $woocommerce;
+    $order = new WC_Order($order_id);
+    $email = method_exists($order, 'get_billing_email') ? $order->get_billing_email() : $order->billing_email;
+    $custo_firstname = method_exists($order, 'get_billing_first_name') ? $order->get_billing_first_name() : $order->billing_first_name;
+    $custo_lastname = method_exists($order, 'get_billing_last_name') ? $order->get_billing_last_name() : $order->billing_last_name;
+    $the_order_id = method_exists($order, 'get_id') ? $order->get_id() : $order->id;
+    $the_order_key = method_exists($order, 'get_order_key') ? $order->get_order_key() : $order->order_key;
+    $requestToken = array(
+      'MerchantKey' => $this->get_option('ovri_gateway_api_key'),
+      'amount' => $order->get_total(),
+      'RefOrder' => $order_id,
+      'Customer_Email' => $email,
+      'Customer_FirstName' => $custo_firstname,
+      'Customer_Name' => $custo_lastname,
+      'urlOK' => get_site_url() . '/?wc-api=wc_ovri_return&mtg_ord=' . $order_id . '',
+      'urlKO' => get_site_url() . '/?wc-api=wc_ovri_return&mtg_ord=' . $order_id . '',
+      'urlIPN' => get_site_url() . '/?wc-api=wc_ovri',
+    );
+    $getToken = $this->getToken($requestToken);
+    if (!is_wp_error($getToken)) {
+      $results = json_decode($getToken['body'], true);
+      if ($getToken['response']['code'] === 200) {
+        wc_add_notice(__('Ovri : ' . $results['Error_Code'] . ' - ' . $results['Short_Description'] . ' - ' . $results['Full_Description'] . '', 'ovri'), 'error');
+        return;
+      } else if ($getToken['response']['code'] === 400) {
+        wc_add_notice(__('Ovri : ' . $results['ErrorCode'] . ' - ' . $results['ErrorDescription'] . '', 'ovri'), 'error');
+        return;
+      } else if ($getToken['response']['code'] === 201) {
+        return array(
+          'result' => 'success',
+          'redirect' => ovri_universale_params()['WebUriStandard'] . $results['SACS']
+        );
+      } else {
+        wc_add_notice('Ovri : Connection error', 'error');
+        return;
+      }
+    } else {
+      wc_add_notice('Ovri : Connection error', 'error');
+      return;
+    }
+  }
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,125 @@
+=== OVRI Banking ===
+Author: OVRI SAS
+Author URI: https://www.ovri.com/
+Contributors: ovribanking
+Tags: payment,payments,payment gateway,payment processor,payment processing,checkout,payment pro,merchant account,contrat vad,moyen de paiement,card,credit card,paiement,bezahlsystem,purchase,online payment,ipspayment,ips payment,moneytigo,ovri,ovri banking,stripe,easytransac,payzen,paypal,adyen,mangopay,mollie
+Requires at least: 4.1
+Tested up to: 6.0
+Requires PHP: 7.1
+Stable tag: 1.5.2
+License: GPLv2 or later
+WC Tested up to: 6.0
+WC requires at least: 2.6
+
+🤩 Quickly add OVRI payment forms to accept credit cards in minutes - #1 best Ovri Banking payments plugin! 🚀
+
+== Description ==
+
+= The Best OVRI Payment Forms and Ovri Banking Payments Plugin for WordPress =
+
+The best <a href="https://www.ovri.com/?utm_source=wordpress-plugin-listing&utm_campaign=wordpress&utm_medium=marketplaces" target="_blank">OVRI Banking</a> payment forms and plugin for WordPress that helps you quickly accept credit card payments to grow your business.
+
+This is a payment plugin that connects the OVRI banking solution to your WORDPRESS site in record time. In just a few steps, you can be up and running in no time. Simply create a payment form and start accepting credit cards and other payment types on your WordPress site.
+
+<a href="https://www.ovri.com/?utm_source=wordpress-plugin-listing&utm_campaign=wordpress&utm_medium=marketplaces" target="_blank">Ovri Payment</a> is a drop-in payment flow optimized for the highest conversion. It’s built for desktop, tablet, and mobile devices. It’s continuously tested and updated to offer a frictionless payment experience. It’s also PCI DSS compliant and SCA-ready without any changes to your website.
+
+== Installation ==
+
+These steps should be made for module's correct work:
+
+1. <a href="https://my.ovri.app" target="_blank">Open a account</a> (Registration is done online and the account is opened immediately).
+2. Add your website to <a href="https://my.ovri.app" target="_blank">Ovri DashBord</a> (from your dashboard to get your API key credentials)
+2. Install and adjust the module
+
+[Important] - next steps presume you already have WooCommerce module installed on your website:
+
+1. Module's installation. Choose "Plugins -> Add new" in admin's console, press "Upload Plugin" button, choose zip-archive with plugin and then press "Install".
+2. Adjustment. Choose "WooCommerce -> Settings" in admin's console and proceed to "Payments" tab. Choose "Ovri" in payment gateways list and proceed to settings.
+Fill in "Api Key" and "Secret Key" - these values can be found in https://my.ovri.app. You can leave the rest settings as they go.
+3. After saving your settings, you will have OVRI payments available on your website.
+
+
+== Frequently Asked Questions ==
+
+= What does the plugin do? =
+
+The OVRI plugin adds to your Woocommerce store and to Wordpress the interfacing of your Ovri Banking payment account directly on your store without any particular development.
+
+= What is Ovri ?=
+
+<a href="https://www.ovri.com/?utm_source=wordpress-plugin-listing&utm_campaign=wordpress&utm_medium=marketplaces" target="_blank">OVRI</a> is an online payment gateway that allows you to accept credit cards in a matter of moments.
+
+== Upgrade notice ==
+
+Please note that the OVRI plugin requires a minimum PHP version of 7.1
+
+== Screenshots ==
+
+1. A unique payment experience
+
+== Changelog ==
+= 1.5.2 =
+* Fix bug on admin page
+
+= 1.5.1 =
+* Add language :
+1. French
+1. English
+1. Italian
+1. Spanish
+1. German
+
+= 1.5.0 =
+* Change of name from MoneyTigo to OVRI
+* Test compatibility with WordPress 6.x
+* Fixed new API change
+
+= 1.1.1 =
+* Compatibility test with Wordpress 5.8 and validation
+* Compatibility test with the latest version of WOCOMMERCE 5.4 and validation
+* Fixed of MoneyTigo API call with rectification of payer's last name
+
+= 1.1.0 =
+* Correction of the version management bug
+* Modification in case of refused payment, forcing the creation of a new order at each payment attempt
+* Fixed bug with duplicate stock increment
+* Removal of the moneytigo footer
+* Update check native to wordpress abandon manual check
+* Switch to automatic update by default for the moneytigo module
+
+= 1.0.9 =
+* Solved redirection problem for orders with completed status
+
+= 1.0.8 =
+* Fix bug in list payment method
+
+= 1.0.7 =
+* Fix bug in list payment method
+
+= 1.0.6 =
+* Add spanish translate
+* Update french translate
+* Fix domain text
+* Fix checking moneytigo
+
+= 1.0.5 =
+* Added 2 and 4 times payment methods
+
+= 1.0.4 =
+* Removal of the integrated mode and change of api version
+* Standardization of the module with the latest security rules
+* Optimization of processing and response times
+
+= 1.0.3 =
+* Choice between integrated mode and redirection mode
+* In the integrated reminder mode of the amount to be paid
+* Redirection in case of payment in installments
+
+= 1.0.2 =
+* Modified termination url
+
+= 1.0.1 =
+* Encryption of notifications
+
+= 1.0.0 =
+* WooCommerce Payment Gateway.
